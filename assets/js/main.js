71887dc1511544a71dc655a7f7b330c676d23f05
Aakarsh507 <102616783+Aakarsh507@users.noreply.github.com>
2022-09-17 21:51:37 -0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,9 +130,7 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: false,
-      typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 80,
     });
   }
 
